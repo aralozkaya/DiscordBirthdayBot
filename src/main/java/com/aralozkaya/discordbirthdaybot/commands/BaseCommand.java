f21e72c29e6ec6104dea33a746f19527c469626b
İbrahim Aral Özkaya <41003972+aralozkaya@users.noreply.github.com>
2024-09-04 22:52:38 +0300

    Added more standard fields to the Base Command interface and created a default build method

--- src/main/java/com/aralozkaya/discordbirthdaybot/commands/BaseCommand.java
+++ src/main/java/com/aralozkaya/discordbirthdaybot/commands/BaseCommand.java
@@ -1,17 +1,30 @@
 package com.aralozkaya.discordbirthdaybot.commands;
 
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import discord4j.discordjson.json.ApplicationCommandOptionData;
 import discord4j.discordjson.json.ApplicationCommandRequest;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @Component
 public interface BaseCommand {
     String getName();
 
+    String getDescription();
+
     Boolean isAdminCommand();
+
+    List<ApplicationCommandOptionData> getOptions();
 
     Mono<Void> handle(ChatInputInteractionEvent event);
 
-    ApplicationCommandRequest build();
+    default ApplicationCommandRequest build() {
+        return ApplicationCommandRequest.builder()
+                .name(getName())
+                .description(getDescription())
+                .addAllOptions(getOptions())
+                .build();
+    };
 }
