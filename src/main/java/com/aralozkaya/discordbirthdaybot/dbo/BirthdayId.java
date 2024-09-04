90a6ff9ba4e6811192d6baf6ef9f59084b7d199b
İbrahim Aral Özkaya <41003972+aralozkaya@users.noreply.github.com>
2024-09-04 22:52:37 +0300

    Added constructors for some DBOs

--- src/main/java/com/aralozkaya/discordbirthdaybot/dbo/BirthdayId.java
+++ src/main/java/com/aralozkaya/discordbirthdaybot/dbo/BirthdayId.java
@@ -2,13 +2,17 @@ package com.aralozkaya.discordbirthdaybot.dbo;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.Hibernate;
 
 import java.io.Serial;
 import java.util.Objects;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 @Embeddable
