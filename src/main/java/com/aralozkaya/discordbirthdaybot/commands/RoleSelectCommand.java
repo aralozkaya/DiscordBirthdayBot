c8539236b92bea69edc9636d409dd61c911d17fa
İbrahim Aral Özkaya <41003972+aralozkaya@users.noreply.github.com>
2024-09-08 00:06:54 +0300

    Made sure the bot role is higher than the birthday role since otherwise it can't assign it to users

--- src/main/java/com/aralozkaya/discordbirthdaybot/commands/RoleSelectCommand.java
+++ src/main/java/com/aralozkaya/discordbirthdaybot/commands/RoleSelectCommand.java
@@ -1,11 +1,15 @@
 package com.aralozkaya.discordbirthdaybot.commands;
 
 import com.aralozkaya.discordbirthdaybot.dbo.AssignedRole;
+import com.aralozkaya.discordbirthdaybot.dbo.Guild;
 import com.aralozkaya.discordbirthdaybot.repositories.AssignedRolesRepository;
+import com.aralozkaya.discordbirthdaybot.repositories.GuildsRepository;
+import discord4j.common.util.Snowflake;
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
 import discord4j.core.object.command.ApplicationCommandInteractionOption;
 import discord4j.core.object.command.ApplicationCommandInteractionOptionValue;
 import discord4j.core.object.command.ApplicationCommandOption;
+import discord4j.core.object.entity.PartialMember;
 import discord4j.core.object.entity.Role;
 import discord4j.discordjson.json.ApplicationCommandOptionData;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +22,7 @@ import java.util.List;
 @Component
 public class RoleSelectCommand implements BaseCommand {
     private final AssignedRolesRepository assignedRoleRepository;
+    private final GuildsRepository guildsRepository;
 
     @Override
     public String getName() {
@@ -46,26 +51,43 @@ public class RoleSelectCommand implements BaseCommand {
 
     @Override
     public Mono<Void> handle(ChatInputInteractionEvent event) {
-        Role role = event.getOption("role")
+        Role selectedRole = event.getOption("role")
                 .flatMap(ApplicationCommandInteractionOption::getValue)
                 .map(ApplicationCommandInteractionOptionValue::asRole)
                 .get()
                 .block();
 
-        Long guildID = role.getGuildId().asLong();
-        Long roleID = role.getId().asLong();
+        Snowflake guildID = selectedRole.getGuildId();
 
-        assignedRoleRepository.findById(guildID)
+        Role botRole = event.getClient()
+                .getSelfMember(guildID)
+                .flatMap(PartialMember::getHighestRole)
+                .block();
+
+        if(botRole.getPosition().block() < selectedRole.getPosition().block()) {
+            return event.reply()
+                    .withContent("You can't select a role higher than the bot's role! " +
+                    "Go to the server settings and put the targeted birthday role below the bot role")
+                    .withEphemeral(true);
+        }
+
+        Long guildIDLong = guildID.asLong();
+        Long roleIDLong = selectedRole.getId().asLong();
+
+        assignedRoleRepository.findById(guildIDLong)
                 .ifPresentOrElse(
                         assignedRole -> {
-                            assignedRole.setRoleId(roleID);
+                            assignedRole.setRoleId(roleIDLong);
                             assignedRoleRepository.save(assignedRole);
                         },
                         () -> {
-                            AssignedRole assignedRole = new AssignedRole(guildID, roleID);
+                            Guild guild = guildsRepository.findById(guildIDLong).orElseThrow();
+                            AssignedRole assignedRole = new AssignedRole(guildIDLong, guild, roleIDLong);
                             assignedRoleRepository.save(assignedRole);
                         }
                 );
-        return event.reply("Role: " + role.getName() + " is selected!");
+        return event.reply()
+                .withContent("Role: " + selectedRole.getName() + " is selected!")
+                .withEphemeral(true);
     }
 }
