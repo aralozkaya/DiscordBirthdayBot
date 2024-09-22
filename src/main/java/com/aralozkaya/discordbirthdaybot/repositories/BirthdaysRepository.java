ffa20f8274a936f738719b1f4e6dcaa5bd655451
İbrahim Aral Özkaya <41003972+aralozkaya@users.noreply.github.com>
2024-09-22 22:43:33 +0300

    Added a custom database query method to list only the birthday data of those who do not have a role assigned currently.

--- src/main/java/com/aralozkaya/discordbirthdaybot/repositories/BirthdaysRepository.java
+++ src/main/java/com/aralozkaya/discordbirthdaybot/repositories/BirthdaysRepository.java
@@ -2,11 +2,16 @@ package com.aralozkaya.discordbirthdaybot.repositories;
 
 import com.aralozkaya.discordbirthdaybot.dbo.Birthday;
 import com.aralozkaya.discordbirthdaybot.dbo.Birthday.BirthdayId;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BirthdaysRepository extends CrudRepository<Birthday, BirthdayId> {
-    Iterable<Birthday> getAllByGuild_Enabled (Boolean enabled);
+    @Query("SELECT b FROM Birthday b WHERE b.guild.enabled = :enabled AND b.id.userId not in (SELECT cba.id.userId FROM CurrentBirthdayAssignee cba WHERE cba.id.guildId = b.id.guildId)")
+    List<Birthday> getAllNotCurrentlyAssignedBirthdaysByGuild_Enabled (@Param("enabled") boolean enabled);
     void deleteById_GuildIdAndId_UserId(Long id_guildId, Long id_userId);
 }
